ae9c3af73b82816b8eeb3113a2fe4452b216f7c0
tinSn0703 <condensa1952@gmail.com>
2016-07-26 17:56:39 +0900

    0.2.0
    関数に説明。メンバ変数をややいじった。特に意味のない関数を消した

--- H28_i_class/H28_I_C_AD.cpp
+++ H28_i_class/H28_I_C_AD.cpp
@@ -14,8 +14,12 @@ AD変換を行うクラス。ポテンジョンメータの値を読むことが
 #include "H28_i_class.h"
 
 class C_AD
+/*
+AD変換を扱うクラス
+*/
 {	
 	protected:
+	
 	union U_AD_MUX
 	{
 		struct S_AD_MUX
@@ -25,23 +29,25 @@ class C_AD
 			BOOL _mux2 :1;
 			BOOL _mux3 :1;
 			BOOL _mux4 :1;
+			
 #ifdef _AVR_IOM640_H_
 			BOOL _mux5 :1;
 #endif
+		
 		};
 		
-		S_AD_MUX _mux_bit;		
+		S_AD_MUX _mux_bit;
+		usint _ad_num :5;
+		
 #if defined(_AVR_IOM640_H_)
-		usint _mux_admux :5;
-		E_AD_NUM _ad_num :6;
+		E_AD_NUM _mux_admux :6;
 #elif defined(_AVR_IOM164_H_)
 		E_AD_NUM _mux_admux :5;
 #endif
+
 	};
 	
 	U_AD_MUX _mem_ad;
-	
-	void Set_num(E_AD_NUM );
 	
 	void Set_first();
 	void Set(E_AD_NUM ,BOOL );
@@ -53,38 +59,35 @@ class C_AD
 	usint Do();
 };
 
-//protected
-inline void 
-C_AD::
-Set_num (E_AD_NUM _arg_ad_num)
-{
-#if defined(_AVR_IOM640_H_)
-	_mem_ad._ad_num = _arg_ad_num;
-#elif defined(_AVR_IOM164_H_)
-	_mem_ad._mux_admux = _arg_ad_num;
-#endif
-}
+//protected member
 
+/**
+ * \brief : 最初の最初の設定
+ */
 inline void 
 C_AD::
 Set_first ()
 {
-	static BOOL _sta_ad_first = FALES;
+	static BOOL _sta_ad_flag_first = FALES;
 	
-	if (_sta_ad_first == FALES)
+	if (_sta_ad_flag_first == FALES)
 	{
 		ADMUX  = (1<<REFS0);
 		ADCSRA = ((1 << ADEN) | (1 << ADPS1) | (1 << ADPS0));
 		ADCSRB = 0;
 		DIDR0  = 0;
+		
 #ifdef _AVR_IOM640_H_
 		DIDR2  = 0;
 #endif
 		
-		_sta_ad_first = TRUE;
+		_sta_ad_flag_first = TRUE;
 	}
 }
 
+/**
+ * \brief : コンストラクタの中身。引数はコンストラクタ参照
+ */
 inline void 
 C_AD::
 Set
@@ -94,61 +97,70 @@ Set
 )
 {
 	Set_first();
-	
-	Set_num(_arg_ad_num);
+
+	_mem_ad._mux_admux = _arg_ad_num;
 	
 #if defined(_AVR_IOM640_H_)
 	switch (_arg_ad_io_turn)
 	{
 		case TRUE:
 		{
-			DDRF  &= ~(TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._mux_admux);
-			PORTF |=  (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._mux_admux);
+			DDRF  &= ~(TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._ad_num);
+			PORTF |=  (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._ad_num);
 			
-			DDRK  &= ~(_mem_ad._mux_bit._mux5 << _mem_ad._mux_admux);
-			PORTK |=  (_mem_ad._mux_bit._mux5 << _mem_ad._mux_admux);
+			DDRK  &= ~(_mem_ad._mux_bit._mux5 << _mem_ad._ad_num);
+			PORTK |=  (_mem_ad._mux_bit._mux5 << _mem_ad._ad_num);
 			
 			break;
 		}
 		case FALES:
 		{
-			DDRF  |=  (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._mux_admux);
-			PORTF &= ~(TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._mux_admux);
+			DDRF  |=  (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._ad_num);
+			PORTF &= ~(TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._ad_num);
 			
-			DDRK  |=  (_mem_ad._mux_bit._mux5 << _mem_ad._mux_admux);
-			PORTK &= ~(_mem_ad._mux_bit._mux5 << _mem_ad._mux_admux);
+			DDRK  |=  (_mem_ad._mux_bit._mux5 << _mem_ad._ad_num);
+			PORTK &= ~(_mem_ad._mux_bit._mux5 << _mem_ad._ad_num);
 			
 			break;
 		}
 	}
 
-	DIDR0 |= (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._mux_admux);
-	DIDR2 |= (_mem_ad._mux_bit._mux5 << _mem_ad._mux_admux);
+	DIDR0 |= (TURN_TF(_mem_ad._mux_bit._mux5) << _mem_ad._ad_num);
+	DIDR2 |= (_mem_ad._mux_bit._mux5 << _mem_ad._ad_num);
 #elif defined(_AVR_IOM164_H_)
 	switch (_arg_ad_io_turn)
 	{
 		case TRUE:
 		{
-			DDRA  &= ~(1 << _mem_ad._mux_admux);
-			PORTA |=  (1 << _mem_ad._mux_admux);
+			DDRA  &= ~(1 << _mem_ad._ad_num);
+			PORTA |=  (1 << _mem_ad._ad_num);
 			
 			break;
 		}
 		case FALES:
 		{
-			DDRA  |=  (1 << _mem_ad._mux_admux);
-			PORTA &= ~(1 << _mem_ad._mux_admux);
+			DDRA  |=  (1 << _mem_ad._ad_num);
+			PORTA &= ~(1 << _mem_ad._ad_num);
 			
 			break;
 		}
 	}
 
-	DIDR0 |= (1 << _mem_ad._mux_admux);
+	DIDR0 |= (1 << _mem_ad._ad_num);
 #endif
+
 }
 
-//public
-inline 
+//public member
+
+/**
+ * \brief : コンストラクタ
+ * 
+ * \param _arg_ad_num : 使う番号
+ * \param _arg_ad_io_turn : IOピンの設定。うまくいかないときに変えたりして
+ *		TRUE  -> OUT
+ *		FALES -> IN
+ */
 C_AD::
 C_AD
 (
@@ -159,6 +171,11 @@ C_AD
 	Set(_arg_ad_num, _arg_ad_io_turn);
 }
 
+/**
+ * \brief AD変換を実行する
+ * 
+ * \return usint : 実行結果のデータ。10bitの0~1023まで
+ */
 inline usint 
 C_AD::
 Do ()
